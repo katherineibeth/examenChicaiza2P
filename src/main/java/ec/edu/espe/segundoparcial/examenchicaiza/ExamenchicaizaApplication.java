0b6f99d4e72f05145d65b7a3928121ced56c3aed
katherineibeth <kichicaiza@espe.edu.ec>
2024-02-05 19:08:25 -0500

    Configuracion del Proyecto

---
+++ src/main/java/ec/edu/espe/segundoparcial/examenchicaiza/ExamenchicaizaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examenchicaiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenchicaizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenchicaizaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/segundoparcial/examenchicaiza/ExamenchicaizaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.segundoparcial.examenchicaiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenchicaizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
